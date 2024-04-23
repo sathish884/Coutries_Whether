88c4dbd3df64260ea4da8c6b511df9c0b6d955a2
Baskar7499 <baskar.i@logikoof.in>
2024-04-24 00:15:38 +0530

    updates

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Coutries_Whether
+
+Using Rest Countries api and show the details of card inside Country name, Population, Region, LatLang, Country code and Weather of the country
+
+Test case was done 
+
+Deployed Successfully done
+
+Github Link => 
+
+Netlify Link (Deployed Link) => 
+
+Test Script => <script src="https://app.zenclass.in/sheets/v1/js/zen/suite/bundle.js"></script>
--- Whether_Task/script.js
+++ Whether_Task/script.js
@@ -10,63 +10,77 @@ function createElements(tagName, classes = [], textContent = '') {
     return element;
 }
 
-const container = createElements('div', ['container', 'p-5']);
+const container = createElements('div', ['container']);
+
+const h1Tag = createElements('h1', ['text-center'], 'DOM Manipulation of RestCountries');
+h1Tag.setAttribute('id', 'title');
+
+container.appendChild(h1Tag);
 
 fetch('https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json')
     .then((response) => {
         return response.json();
     })
     .then((data) => {
-        let rowCount = Math.ceil(data.length / 4);
+        let rowCount = Math.ceil(data.length / 3);
 
         for (let i = 0; i < rowCount; i++) {
-            let row = createElements('div', ['row', 'm-3']);
+            let row = createElements('div', ['row']);
 
-            for (let j = 0; j < 4; j++) {
-                let dataIndex = i * 4 + j;
+            for (let j = 0; j < 3; j++) {
+                let dataIndex = i * 3 + j;
                 if (dataIndex < data.length) {
                     let element = data[dataIndex];
 
-                    let column = createElements('div', ['col-lg-3', 'col-sm-12']);
+                    console.log(element);
 
-                    let card = createElements('div', ['card', 'border-5']);
+                    let column = createElements('div', ['col-sm-6', 'col-md-4', 'col-lg-4', 'col-xl-4']);
+
+                    let card = createElements('div', ['card', 'border-5', 'h-100']);
                     let cardHead = createElements('div', ['card-header', 'p-3', 'text-center', 'bg-dark', 'text-white'], element.name);
                     let cardBody = createElements('div', ['card-body', 'text-center']);
 
-                    let flagImage = createElements('img', ['img-thumbnail']);
+                    let flagImage = createElements('img', ['img-thumbnail', 'mb-3', 'card-img-top']);
                     flagImage.setAttribute('src', element.flag);
                     flagImage.setAttribute('alt', element.name);
-                    flagImage.setAttribute('width', '100');
-                    flagImage.setAttribute('height', 'auto');
+                    flagImage.setAttribute('width', '200');
+                    flagImage.setAttribute('height', '100');
 
                     cardBody.appendChild(flagImage);
 
-                    let countryCode = createElements('p', [], `Country Code: ${element.alpha3Code}`);
-                    let capital = createElements('p', [], `Capital: ${element.capital}`);
-                    let region = createElements('p', [], `Region: ${element.region}`);
-                    let population = createElements('p', [], `Population: ${element.population.toLocaleString()}`);
+                    let nativeName = createElements('div', ['card-text']);
+                    nativeName.innerText = `Native Name : ${element.nativeName}`;
+                    let region = createElements('div', ['card-text']);
+                    region.innerText = `Region : ${element.region}`;
+                    let population = createElements('div', ['card-text']);
+                    population.innerText = `Population : ${element.population}`;
+                    let countryCode = createElements('div', ['card-text'], `Country Code: ${element.alpha3Code}`);
+                    let capital = createElements('div', ['card-text']);
+                    capital.innerText = `Capital : ${element.capital}`;
+                    let latlang = createElements('div', ['card-text'], `LatLang: (${element.latlng[0]}, ${element.latlng[1]})`);
                     let button = createElements('button', ['btn', 'btn-primary'], 'Check Weather');
 
+                    let countryName = element.name
+
                     button.addEventListener('click', () => {
-                        fetch(`https://openweathermap.org/api/${element.capital}`)
-                            .then((resp) => {
-                                return resp.json();
-                            })
-                            .then((weatherData) => {
-                                console.log(weatherData);
-                                // Display weather data here
-                            })
-                            .catch((err) => {
-                                console.log("Error fetching weather data", err);
-                            });
+                        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${element.name}&appid=ade33abcb26e1db2eba4c11dabe3a801&units=metric`).then((resp) => {
+                            return resp.json();
+                        }).then((weatherData) => {
+                            console.log(weatherData);
+                            alert(`Weather : ${weatherData.weather[0].description}, Temperature : ${weatherData.main.temp} Wind Speed : ${weatherData.wind.speed}`)
+                            // Display weather data here
+                        }).catch((err) => {
+                            console.log("Error fetching weather data", err);
+                        });
                     });
 
-                    cardBody.appendChild(countryCode);
-                    cardBody.appendChild(population);
-                    cardBody.appendChild(capital);
+                    cardBody.appendChild(nativeName);
                     cardBody.appendChild(region);
+                    cardBody.appendChild(population);
+                    cardBody.appendChild(countryCode);
+                    cardBody.appendChild(capital);
+                    cardBody.appendChild(latlang);
                     cardBody.appendChild(button);
-
                     card.appendChild(cardHead);
                     card.appendChild(cardBody);
                     column.appendChild(card);
